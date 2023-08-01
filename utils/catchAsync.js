78a784bcad92accd1d1bf1e1c993fb9d92f4c71d
Joseph Urquhart <josephurquhart4@gmail.com>
2023-08-01 13:35:32 +0100

    updated catchAsync to handle errors better

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,5 +1,17 @@
+// module.exports = (func) => {
+//   return (req, res, next) => {
+//     func(req, res, next).catch(next);
+//   };
+// };
+
 module.exports = (func) => {
   return (req, res, next) => {
-    func(req, res, next).catch(next);
+    func(req, res, next).catch((error) => {
+      // Handle the error here
+      // For example, you can log the error for debugging purposes
+      console.error('Async function error:', error);
+      // Then pass the error to Express error-handling middleware
+      next(error);
+    });
   };
 };
